00e8fd6f63f125c41be6ca7e8d524d7e99efef53
Abraham S <abrahamsalloum@gmail.com>
2020-10-17 13:01:57 +1100

    ind summ

--- src/components/loadTicker/Totals.js
+++ src/components/loadTicker/Totals.js
@@ -1,28 +1,77 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
+import React from 'react'
+import { useSelector} from 'react-redux';
 import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Box from '@material-ui/core/Box'
 
 const Totals = ({showbox}) => {
-  const price = useSelector(state => state.AddTickers.price)
+  const prices = useSelector(state => state.AddTickers.price)
   const tickerlist = useSelector(state => state.AddTickers.tickerlist)
   
   if(!showbox){
     return false
   }
-  return(
-    <Card style={{ width: '100%' }}>
-      <Typography color="textSecondary" gutterBottom>
-        Info
-      </Typography>
-      <CardContent>
-      {showbox.data.ticker}
-      </CardContent>
-    </Card>
-  )
 
+  const getsummaries = (tickerlist, prices) => {
+    let a = 0
+    let d = 0
+    let p = 0
+
+    for(let t in tickerlist){
+      if(tickerlist[t]['ticker'] === showbox.data.ticker){
+        a = a + (Number(tickerlist[t]['amount']) * Number(tickerlist[t]['purchaseprice']))
+        d = d + (Number(tickerlist[t]['amount']))
+      }
+    }
+
+    for(let i in prices){
+      if(prices[i]["ticker"] === showbox.data.ticker){
+        p = Number(prices[i]['price'])
+        break
+      }
+    }
+    return [a,d,p]
+  }
+
+  const amounts = getsummaries(tickerlist, prices)
+  return(
+    <Box m={1}>
+    {showbox.data.ticker}
+        <TableContainer component={Paper}>
+          <Table size="small" aria-label="a list">
+            <TableHead>
+              <TableRow>
+                <TableCell>Type</TableCell>
+                <TableCell>Value</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>Total Units</TableCell>
+                <TableCell>{amounts[1]}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Avg. Purchase Price</TableCell>
+                <TableCell>${amounts[0]/amounts[1]}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Total Value</TableCell>
+                <TableCell>${amounts[0]}</TableCell>
+              </TableRow>
+              <TableRow>
+              <TableCell>Gain</TableCell>
+              <TableCell>${amounts[2]*amounts[1] - amounts[0]}</TableCell>
+            </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+  )
 }
 
 export default Totals
--- src/components/loadTicker/tickerlinechart.js
+++ src/components/loadTicker/tickerlinechart.js
@@ -1,6 +1,6 @@
 
 import React, {useEffect} from 'react'
-import {LineChart, Line, Brush, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from 'recharts';
+import {LineChart, Line, Brush, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts';
 import { useSelector, useDispatch } from 'react-redux';
 import { getLineData } from '../../redux/actions.js'
 
@@ -17,7 +17,7 @@ const SimpleLineChart = () => {
         <LineChart data={chartdata} margin={{top: 0, right: 0, left: 0, bottom: 0}}>
           <YAxis domain={['auto', 'auto']}/>
           <CartesianGrid strokeDasharray="3 3"/>
-          <Legend />
+          
           <Tooltip/>
           <Line type="monotone" dataKey="close" stroke="#8884d8" dot={false} />
           <Brush dataKey="timestamp" travellerWidth={15} />
